0bb25afbd4f01bb05e00f0ec7ecf6a18642232e4
Nergiz Şengün <nergizduygu56@gmail.com>
2025-04-07 17:39:35 +0300

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
+import Player from "./components/Player.jsx"
 
 function App() {
   
 
   return (
 
-    <div id="game-container">
-      <ol id="players" >
-        <li>
-          <span className="player-name" >Player 1</span>
-          <span className="player-symbol" >X</span>
-        </li>
-        <li>
-          <span className="player-name" >Player 2</span>
-          <span className="player-symbol" >O</span>
-        </li>
+    <main> 
+      <div id="game-container">
+        <ol id="players" >
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="Y" />
         </ol>
-        GAME BOARD
-    </div>
-
+          GAME BOARD
+      </div>
+      LOG
+    </main>
   )
 }
 
