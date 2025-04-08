fa65f5b05b123240a3de0f3048360c23d60c4035
Nergiz Şengün <nergizduygu56@gmail.com>
2025-04-08 17:58:06 +0300

    user name edits

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({ initialName, symbol}) {
 
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name" >{name} </span>;
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
+    let editTablePlayerName = <span className="player-name" >{playerName} </span>;
     //let btnCaption = 'Edit';
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editTablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+        // edittablePlayerName = <input type="text" required defaultValue={name} />; yukarıda farklı bir yaklaşım kullanacağı<
        // btnCaption = 'Save';
     }
 
     return(
         <li>
         <span className="player" >
-            {playerName}
+            {editTablePlayerName}
           <span className="player-symbol" >{symbol} </span>
           </span>
           <button onClick={handleEditClick} className="edit">{isEditing ? 'Save' : 'Edit'} </button>
