9037f0403a78b36e2f638de19a0865b1a9527532
Nergiz Şengün <nergizduygu56@gmail.com>
2025-04-09 16:40:09 +0300

    Update Game Board Settings

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -6,13 +8,25 @@ const initialGameBoard = [
 
 
 export default function GameBoard(){
+
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) => {
+            const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];// burada eski elamanları da "..."bu operatör sayesinde ekleyebiliyoruz.
+            updateBoard[rowIndex][colIndex] = 'X';
+            return updateBoard;
+        });
+    }
+
     return(
 
         <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
-            {row.map((playerSymbol, colIndex) => <li key={colIndex}> <button>{playerSymbol}</button></li>)}
+            {row.map((playerSymbol, colIndex) => <li key={colIndex}> <button onClick= {() => {handleSelectSquare(rowIndex, colIndex)}}  >{playerSymbol}</button></li>)}
         </ol>
+
        </li>
         )}
         </ol>
