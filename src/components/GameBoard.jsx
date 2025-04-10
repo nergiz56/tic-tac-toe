796f7f5fb1740e9b7e14b87cf7f80b933c6ac36e
Nergiz Şengün <nergizduygu56@gmail.com>
2025-04-10 12:07:21 +0300

    placing symbols in a table

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -25,7 +25,9 @@ export default function GameBoard({onSelectSquare,  activePlayerSymbol}){
         <ol id="game-board">
         {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
-            {row.map((playerSymbol, colIndex) => <li key={colIndex}> <button onClick= {() => {handleSelectSquare(rowIndex, colIndex)}} >{playerSymbol}</button></li>)}
+            {row.map((playerSymbol, colIndex) => 
+            <li key={colIndex}> 
+            <button onClick= {() => {handleSelectSquare(rowIndex, colIndex)}} >{playerSymbol}</button></li>)}
         </ol>
 
        </li>
